c944406ff97688fd5a55b07a8faf406ee7719d84
GalileaRestrepo <a01178273@tec.mx>
2026-03-04 15:18:17 -0600

    Initial Commit

---
+++ LakersHomecourt Watch App/LakersHomecourtApp.swift
@@ -0,0 +1,17 @@
+//
+//  LakersHomecourtApp.swift
+//  LakersHomecourt Watch App
+//
+//  Created by AGRM  on 04/03/26.
+//
+
+import SwiftUI
+
+@main
+struct LakersHomecourt_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
